d920df8455fb3759d5efd7e3c99d8a9bb5214cf9
Neil Terry <NeilTerry@gmail.com>
2014-01-02 12:50:25 -0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Neil and Beth's Wedding</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Include Analytics tracking stuff -->
+</head>
+
+
+<body id="home">
+
+    <div class="banner">
+            <h1>Neil and Beth's Wedding</h1>
+            <h2>June 28, 2014</h2>
+
+
+    <!--Navigation Menu-->
+    <?php include 'include/nav2.php';?>
+    
+    </div>
+
+</body>
+
+</html>
