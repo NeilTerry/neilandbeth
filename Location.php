e3da79612498f5afa8f99cdcd6a0e9b3dabf63d8
Neil Terry <NeilTerry@gmail.com>
2014-01-03 14:30:52 -0500

    Added page framework

---
+++ Location.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Neil and Beth's Wedding</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Include Analytics tracking stuff -->
+</head>
+
+
+<body id="home">
+
+    <div class="banner">
+    
+    <!--Banner-->
+    <?php include 'include/banner.html';?>
+
+    <!--Navigation Menu-->
+    <?php include 'include/nav2.php';?>
+    
+    </div>
+
+    <div class="content">
+
+        <?php include 'include/hometext.html';?>
+
+    </div>
+
+</body>
+
+</html>
