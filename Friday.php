d92cd4f0cb93db0bc328ed78fd17c01a79bbfc1a
Neil Terry <NeilTerry@gmail.com>
2014-01-08 21:49:35 -0500

    Added Friday and Registry Page

---
+++ Friday.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Neil and Beth's Wedding</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Include Analytics tracking stuff -->
+    <?php include 'tracking.php';?>
+</head>
+
+
+<body id="home">
+
+    
+    <!--Banner-->
+    <?php include 'include/banner.html';?>
+
+    <!--Navigation Menu-->
+    <?php include 'include/nav2.php';?>
+    
+
+    <div class="content">
+
+        <p>Under Construction...</p>
+
+    </div>
+
+</body>
+
+</html>
